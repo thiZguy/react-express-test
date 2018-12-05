53a8bf317d6ce04d6aa5b22dfc6d745ad335d6db
Santiago <santiagojmontero@outlook.com>
2018-12-04 21:40:52 -0430

    added function to list using fetch on frontend

--- client/src/components/CountryCard.js
+++ client/src/components/CountryCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import placeholder from '../placeholder.svg';
+import placeholder from '../assets/placeholder.svg';
 import PropTypes from 'prop-types';
 import '../App.css';
 
